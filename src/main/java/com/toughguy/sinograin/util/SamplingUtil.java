29e1292e6706c31c789f2701ba9e4eca77f28a9f
BOBO <1192012793@qq.com>
2018-02-27 17:44:48 +0800

    解决冲突

--- src/main/java/com/toughguy/sinograin/util/SamplingUtil.java
+++ src/main/java/com/toughguy/sinograin/util/SamplingUtil.java
@@ -1,8 +1,12 @@
 package com.toughguy.sinograin.util;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Properties;
+
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * 扦样编号工具类
@@ -12,11 +16,23 @@ import java.util.Properties;
 public class SamplingUtil {
 	
 	
-	public static String SampleNumber(String libraryName,String sort) {
+	
+	public String SampleNumber(String libraryName,String sort) throws IOException {
 	 String lastSampleNo;
 	 String ranStr = null;
-		try {
-			InputStream inStream = SamplingUtil.class.getClassLoader().getResourceAsStream("config/grain.properties"); 
+	 String rootPath =getClass().getResource("/").getFile().toString();
+	 String [] path = rootPath.split("/");
+	 String p = null;
+	for(String s:path){
+		if(StringUtils.isEmpty(p)){
+			p = s+"/";
+		}
+		p = p+s+"/";
+		if("target".equals(s)){
+			break;
+		}
+	}
+		InputStream	inStream = SamplingUtil.class.getClassLoader().getResourceAsStream("config/grain.properties"); 
 			Properties prop = new Properties(); 
 			prop.load(inStream);
 			lastSampleNo = prop.getProperty("grain.sampleNo");
@@ -25,12 +41,13 @@ public class SamplingUtil {
 			}else{
 				ranStr = (Integer.parseInt(lastSampleNo.trim()) + 1) + "";
 			}
-			ranStr = String.format("%04d", Integer.parseInt(ranStr));
+			OutputStream out = new FileOutputStream(p+"classes/config/grain.properties");  
 			prop.setProperty("grain.sampleNo", ranStr);
-		} catch (IOException e) {
-			e.printStackTrace();
-		} 
-		lastSampleNo = libraryName + "-" + sort + "-" + ranStr;
-		return lastSampleNo;
+			prop.store(out,  null); 
+			ranStr = String.format("%04d", Integer.parseInt(ranStr));			
+			lastSampleNo = libraryName + "-" + sort + "-" + ranStr;
+			inStream.close();  
+	        out.close();
+			return lastSampleNo;
 	}
 }
