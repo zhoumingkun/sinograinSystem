ce0b1372824deb35530c5104d05d5ed863eefcaa
zhoumingkun <573206019@qq.com>
2018-03-24 12:50:49 +0800

    添加面筋吸水量测定记录

---
+++ src/main/java/com/toughguy/sinograin/service/barn/impl/MianjinxishuiliangServiceImpl.java
@@ -0,0 +1,11 @@
+package com.toughguy.sinograin.service.barn.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.toughguy.sinograin.model.barn.Mianjinxishuiliang;
+import com.toughguy.sinograin.service.barn.prototype.IMianjinxishuiliangService;
+import com.toughguy.sinograin.service.impl.GenericServiceImpl;
+@Service
+public class MianjinxishuiliangServiceImpl extends GenericServiceImpl<Mianjinxishuiliang, Integer> implements IMianjinxishuiliangService{
+	
+}
---
+++ src/main/java/com/toughguy/sinograin/service/barn/prototype/IMianjinxishuiliangService.java
@@ -0,0 +1,8 @@
+package com.toughguy.sinograin.service.barn.prototype;
+
+import com.toughguy.sinograin.model.barn.Mianjinxishuiliang;
+import com.toughguy.sinograin.service.prototype.IGenericService;
+
+public interface IMianjinxishuiliangService extends IGenericService<Mianjinxishuiliang, Integer>{
+
+}
