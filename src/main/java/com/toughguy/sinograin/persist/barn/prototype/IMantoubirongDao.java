02fb5bec91e2e473feb58fe45dd08cf61bfecc39
zhoumingkun <573206019@qq.com>
2018-03-24 12:52:45 +0800

    添加馒头比容测定记录

---
+++ src/main/java/com/toughguy/sinograin/persist/barn/prototype/IMantoubirongDao.java
@@ -0,0 +1,8 @@
+package com.toughguy.sinograin.persist.barn.prototype;
+
+import com.toughguy.sinograin.model.barn.Mantoubirong;
+import com.toughguy.sinograin.persist.prototype.IGenericDao;
+
+public interface IMantoubirongDao extends IGenericDao<Mantoubirong, Integer>{
+
+}
