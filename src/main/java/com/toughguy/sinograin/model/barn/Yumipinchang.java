fea09c7b89d94a82a60d692375f5aec6906254cb
YAO1017 <17600681017@163.com>
2018-03-26 18:07:54 +0800

    //修改玉米品尝

--- src/main/java/com/toughguy/sinograin/model/barn/Yumipinchang.java
+++ src/main/java/com/toughguy/sinograin/model/barn/Yumipinchang.java
@@ -14,24 +14,21 @@ public class Yumipinchang extends AbstractModel{
 	private static final long serialVersionUID = 4139049675415615508L;
 	
 	private int smallSampleId;           //小样id
-	private String content;				//内容
 	private Date riqi;				    //日期
 	private String pinpingyuan;			//品评员
 	private String table_version;		//表格版本
-	
-	
+	private String wotouqiwei;          //窝头气味
+	private String wotouseze;           //窝头色泽
+	private String waiguanxingzhuang;   //外观形状
+	private String neibuxingzhuang;     //内部形状
+	private String ziwei;               //滋味
+	private String pinchangpingfenzhi;  //品尝评分值
 	
 	public int getSmallSampleId() {
 		return smallSampleId;
 	}
 	public void setSmallSampleId(int smallSampleId) {
 		this.smallSampleId = smallSampleId;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
 	}
 	public Date getRiqi() {
 		return riqi;
@@ -53,6 +50,42 @@ public class Yumipinchang extends AbstractModel{
 	}
 	
 	
+	public String getWotouqiwei() {
+		return wotouqiwei;
+	}
+	public void setWotouqiwei(String wotouqiwei) {
+		this.wotouqiwei = wotouqiwei;
+	}
+	public String getWotouseze() {
+		return wotouseze;
+	}
+	public void setWotouseze(String wotouseze) {
+		this.wotouseze = wotouseze;
+	}
+	public String getWaiguanxingzhuang() {
+		return waiguanxingzhuang;
+	}
+	public void setWaiguanxingzhuang(String waiguanxingzhuang) {
+		this.waiguanxingzhuang = waiguanxingzhuang;
+	}
+	public String getNeibuxingzhuang() {
+		return neibuxingzhuang;
+	}
+	public void setNeibuxingzhuang(String neibuxingzhuang) {
+		this.neibuxingzhuang = neibuxingzhuang;
+	}
+	public String getZiwei() {
+		return ziwei;
+	}
+	public void setZiwei(String ziwei) {
+		this.ziwei = ziwei;
+	}
+	public String getPinchangpingfenzhi() {
+		return pinchangpingfenzhi;
+	}
+	public void setPinchangpingfenzhi(String pinchangpingfenzhi) {
+		this.pinchangpingfenzhi = pinchangpingfenzhi;
+	}
 	@Override
 	public String toString(){
 		return JsonUtil.objectToJson(this);
