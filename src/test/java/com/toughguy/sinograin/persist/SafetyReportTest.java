49d3e1c20211daa2479d225a08a2975cc76dff00
BOBO <1192012793@qq.com>
2018-04-06 11:30:51 +0800

    添加监督检查报告模板和测试类

---
+++ src/test/java/com/toughguy/sinograin/persist/SafetyReportTest.java
@@ -0,0 +1,32 @@
+package com.toughguy.sinograin.persist;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.toughguy.sinograin.SinograinApplication;
+import com.toughguy.sinograin.model.barn.Library;
+import com.toughguy.sinograin.model.barn.SafetyReport;
+import com.toughguy.sinograin.pagination.PagerModel;
+import com.toughguy.sinograin.persist.barn.prototype.ILibraryDao;
+import com.toughguy.sinograin.service.barn.prototype.ISafetyReportService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes=SinograinApplication.class)
+public class SafetyReportTest {
+	
+	@Autowired
+	private ISafetyReportService safetyReportService;
+	
+	@Test
+	public void testReg(){
+		safetyReportService.ExportSafetyReport();
+	}
+
+}
