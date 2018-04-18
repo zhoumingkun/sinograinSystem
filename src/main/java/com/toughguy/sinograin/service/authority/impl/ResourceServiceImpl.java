fceae4dbdf58a91e28a7c101c353a4f9c33ab5a5
YAO1017 <17600681017@163.com>
2018-04-18 17:23:49 +0800

    //11

--- src/main/java/com/toughguy/sinograin/service/authority/impl/ResourceServiceImpl.java
+++ src/main/java/com/toughguy/sinograin/service/authority/impl/ResourceServiceImpl.java
@@ -63,25 +63,27 @@ public class ResourceServiceImpl extends GenericServiceImpl<Resource, Integer> i
 		List<TreeDTO> treeList = new ArrayList<>();
 		List<Operation> list = operationDao.findByRoleId(roleId); // 得到该角色所拥有的操作集合
 		Role role = roleDao.find(roleId);
-		List<Operation> operList = operationDao.findByRoleId(role.getRoleRelyId());  //得到依赖角色的所有操作
+		List<Operation> operList = operationDao.findByRoleId(role.getRoleRelyId()); // 得到依赖角色的所有操作
 		for (Resource r : roles) {
 			if (list.size() > 0) {
+				if (operList.size() > 0) { // 有角色依赖 本身也有权限
 					TreeDTO tree1 = new TreeDTO();
 					tree1.setName(r.getResourceName());
 					tree1.setId(r.getId());
 					tree1.setType(-1);
 					tree1.setIndex(r.getGuid());
-//					List<Resource> rList =((IResourceDao) dao).findById(r.getId()); //查询出资源所拥有的下属资源
-//					for (int j = 0; j < list.size(); j++) {
-//						for (Resource resource : rList) {
-//							if (resource.getId() == list.get(j).getResourceId()) {
-//								tree1.setChecked("true");
-//								break;   //如果该角色有此操作的资源  跳出循环
-//							} else {
-//								tree1.setChecked("false");
-//							}
-//						}
-//					}
+					// List<Resource> rList =((IResourceDao)
+					// dao).findById(r.getId()); //查询出资源所拥有的下属资源
+					// for (int j = 0; j < list.size(); j++) {
+					// for (Resource resource : rList) {
+					// if (resource.getId() == list.get(j).getResourceId()) {
+					// tree1.setChecked("true");
+					// break; //如果该角色有此操作的资源 跳出循环
+					// } else {
+					// tree1.setChecked("false");
+					// }
+					// }
+					// }
 					treeList.add(tree1);
 					if (r.getList().size() != 0) {
 						List<TreeDTO> treeList2 = new ArrayList<>();
@@ -91,15 +93,15 @@ public class ResourceServiceImpl extends GenericServiceImpl<Resource, Integer> i
 							tree2.setId(r1.getId());
 							tree2.setType(-1);
 							tree2.setIndex(r1.getGuid());
-//							for (int j = 0; j < list.size(); j++) {
-//								System.out.println(r1.getId()+"-------"+list.get(j).getResourceId());
-//								if (r1.getId() == list.get(j).getResourceId()) {
-//									tree2.setChecked("true");
-//									break;
-//								} else {
-//									tree2.setChecked("false");
-//								}
-//							}
+							// for (int j = 0; j < list.size(); j++) {
+							// System.out.println(r1.getId()+"-------"+list.get(j).getResourceId());
+							// if (r1.getId() == list.get(j).getResourceId()) {
+							// tree2.setChecked("true");
+							// break;
+							// } else {
+							// tree2.setChecked("false");
+							// }
+							// }
 							treeList2.add(tree2);
 							tree1.setChildren(treeList2);
 							if (r1.getList().size() != 0) {
@@ -117,7 +119,7 @@ public class ResourceServiceImpl extends GenericServiceImpl<Resource, Integer> i
 											if (o.getId() == operList.get(i).getId()) {
 												tree3.setDisabled(true);
 												break;
-											}else{
+											} else {
 												tree3.setDisabled(false);
 											}
 										}
@@ -148,7 +150,7 @@ public class ResourceServiceImpl extends GenericServiceImpl<Resource, Integer> i
 									if (o.getId() == operList.get(i).getId()) {
 										tree3.setDisabled(true);
 										break;
-									}else{
+									} else {
 										tree3.setDisabled(false);
 									}
 								}
@@ -165,7 +167,160 @@ public class ResourceServiceImpl extends GenericServiceImpl<Resource, Integer> i
 							}
 						}
 					}
+				} else {
+					TreeDTO tree1 = new TreeDTO();
+					tree1.setName(r.getResourceName());
+					tree1.setId(r.getId());
+					tree1.setType(-1);
+					tree1.setIndex(r.getGuid());
+					// List<Resource> rList =((IResourceDao)
+					// dao).findById(r.getId()); //查询出资源所拥有的下属资源
+					// for (int j = 0; j < list.size(); j++) {
+					// for (Resource resource : rList) {
+					// if (resource.getId() == list.get(j).getResourceId()) {
+					// tree1.setChecked("true");
+					// break; //如果该角色有此操作的资源 跳出循环
+					// } else {
+					// tree1.setChecked("false");
+					// }
+					// }
+					// }
+					treeList.add(tree1);
+					if (r.getList().size() != 0) {
+						List<TreeDTO> treeList2 = new ArrayList<>();
+						for (Resource r1 : r.getList()) {
+							TreeDTO tree2 = new TreeDTO();
+							tree2.setName(r1.getResourceName());
+							tree2.setId(r1.getId());
+							tree2.setType(-1);
+							tree2.setIndex(r1.getGuid());
+							// for (int j = 0; j < list.size(); j++) {
+							// System.out.println(r1.getId()+"-------"+list.get(j).getResourceId());
+							// if (r1.getId() == list.get(j).getResourceId()) {
+							// tree2.setChecked("true");
+							// break;
+							// } else {
+							// tree2.setChecked("false");
+							// }
+							// }
+							treeList2.add(tree2);
+							tree1.setChildren(treeList2);
+							if (r1.getList().size() != 0) {
+								tree(r1.getList(), roleId);
+							} else if (r1.getOperationList().size() != 0) {
+								List<TreeDTO> treeList3 = new ArrayList<>();
+								for (Operation o : r1.getOperationList()) {
+									if (r1.getId() == o.getResourceId()) {
+										TreeDTO tree3 = new TreeDTO();
+										tree3.setName(o.getDisplayName());
+										tree3.setId(o.getId());
+										tree3.setType(1);
+										tree3.setIndex(o.getGuid());
+										tree3.setDisabled(false);
+										for (int j = 0; j < list.size(); j++) {
+											if (o.getId() == list.get(j).getId()) {
+												tree3.setChecked(true);
+												break;
+											} else {
+												tree3.setChecked(false);
+											}
+										}
+										treeList3.add(tree3);
+										tree2.setChildren(treeList3);
+									}
+								}
+							}
+						}
+					} else if (r.getOperationList().size() != 0) {
+						List<TreeDTO> treeList3 = new ArrayList<>();
+						for (Operation o : r.getOperationList()) {
+							if (r.getId() == o.getResourceId()) {
+								TreeDTO tree3 = new TreeDTO();
+								tree3.setName(o.getDisplayName());
+								tree3.setId(o.getId());
+								tree3.setType(1);
+								tree3.setIndex(o.getGuid());
+								tree3.setDisabled(false);
+								for (int j = 0; j < list.size(); j++) {
+									if (o.getId() == list.get(j).getId()) {
+										tree3.setChecked(true);
+										break;
+									} else {
+										tree3.setChecked(false);
+									}
+								}
+								treeList3.add(tree3);
+								tree1.setChildren(treeList3);
+							}
+						}
+					}
+				}
 			} else { // 该角色还未拥有资源与操作
+				if (operList.size() > 0) { // 有角色依赖 本身没有权限
+					TreeDTO tree1 = new TreeDTO();
+					tree1.setName(r.getResourceName());
+					tree1.setId(r.getId());
+					tree1.setType(-1);
+					tree1.setIndex(r.getGuid());
+					treeList.add(tree1);
+					if (r.getList().size() != 0) {
+						List<TreeDTO> treeList2 = new ArrayList<>();
+						for (Resource r1 : r.getList()) {
+							TreeDTO tree2 = new TreeDTO();
+							tree2.setName(r1.getResourceName());
+							tree2.setId(r1.getId());
+							tree2.setType(-1);
+							tree2.setIndex(r1.getGuid());
+							treeList2.add(tree2);
+							tree1.setChildren(treeList2);
+							if (r1.getList().size() != 0) {
+								tree(r1.getList(), roleId);
+							} else if (r1.getOperationList().size() != 0) {
+								List<TreeDTO> treeList3 = new ArrayList<>();
+								for (Operation o : r1.getOperationList()) {
+									if (r1.getId() == o.getResourceId()) {
+										TreeDTO tree3 = new TreeDTO();
+										tree3.setName(o.getDisplayName());
+										tree3.setId(o.getId());
+										tree3.setType(1);
+										tree3.setIndex(o.getGuid());
+										for (int i = 0; i < operList.size(); i++) {
+											if (o.getId() == operList.get(i).getId()) {
+												tree3.setDisabled(true);
+												break;
+											} else {
+												tree3.setDisabled(false);
+											}
+										}
+										treeList3.add(tree3);
+										tree2.setChildren(treeList3);
+									}
+								}
+							}
+						}
+					} else if (r.getOperationList().size() != 0) {
+						List<TreeDTO> treeList3 = new ArrayList<>();
+						for (Operation o : r.getOperationList()) {
+							if (r.getId() == o.getResourceId()) {
+								TreeDTO tree3 = new TreeDTO();
+								tree3.setName(o.getDisplayName());
+								tree3.setId(o.getId());
+								tree3.setType(1);
+								tree3.setIndex(o.getGuid());
+								for (int i = 0; i < operList.size(); i++) {
+									if (o.getId() == operList.get(i).getId()) {
+										tree3.setDisabled(true);
+										break;
+									} else {
+										tree3.setDisabled(false);
+									}
+								}
+								treeList3.add(tree3);
+								tree1.setChildren(treeList3);
+							}
+						}
+					}
+				} else {
 					TreeDTO tree1 = new TreeDTO();
 					tree1.setName(r.getResourceName());
 					tree1.setId(r.getId());
@@ -213,6 +368,7 @@ public class ResourceServiceImpl extends GenericServiceImpl<Resource, Integer> i
 							}
 						}
 					}
+				}
 			}
 		}
 		return treeList;
