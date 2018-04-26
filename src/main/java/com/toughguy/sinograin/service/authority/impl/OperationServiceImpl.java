cc7f4d6f64cb1fa6dada3f90625b1c03a53bf686
BOBO <1192012793@qq.com>
2018-04-26 13:43:17 +0800

    封装根据roleid查询所有的操作

--- src/main/java/com/toughguy/sinograin/service/authority/impl/OperationServiceImpl.java
+++ src/main/java/com/toughguy/sinograin/service/authority/impl/OperationServiceImpl.java
@@ -10,9 +10,11 @@ import org.springframework.stereotype.Service;
 
 import com.toughguy.sinograin.model.authority.Operation;
 import com.toughguy.sinograin.model.authority.Resource;
+import com.toughguy.sinograin.model.authority.Role;
 import com.toughguy.sinograin.persist.authority.prototype.IOperationDao;
 import com.toughguy.sinograin.service.authority.prototype.IOperationService;
 import com.toughguy.sinograin.service.authority.prototype.IResourceService;
+import com.toughguy.sinograin.service.authority.prototype.IRoleService;
 import com.toughguy.sinograin.service.impl.GenericServiceImpl;
 
 @Service
@@ -20,6 +22,11 @@ public class OperationServiceImpl extends GenericServiceImpl<Operation, Integer>
 	
 	@Autowired
 	private IResourceService resourceService;
+	@Autowired
+	private IOperationDao operationDao;
+	@Autowired
+	private IRoleService roleService;
+	
 	
 	private List<Operation> operation = new ArrayList<Operation>(); //保存操作集合
 	private List<Resource> resource = new ArrayList<Resource>(); //保存资源集合
@@ -49,5 +56,25 @@ public class OperationServiceImpl extends GenericServiceImpl<Operation, Integer>
 	@Override
 	public void deleteAllByResourceId(int resourceId) {
 		((IOperationDao)dao).deleteAllByResourceId(resourceId);
+	}
+
+	@Override
+	public List<Operation> findAllOperationsByRoleId(int roleId) {
+		System.out.println(roleId);
+		// 获取当前roleid 的 所有操作 ， 并且存入临时存储列表中
+		List<Operation> operationListAll = operationDao.findByRoleId(roleId);
+		// 查找父角色
+		// 	递归调用当前方法 ， 返回结果
+		List<Role> relyRoles = roleService.findRelyRole(roleId);
+		if(relyRoles.size()>0) {
+			for(Role r: relyRoles) {
+				List<Operation> relyOperations = findAllOperationsByRoleId(r.getId());
+				if(relyOperations.size() > 0) {
+					operationListAll.addAll(relyOperations);
+				}
+			}
+		}
+		// 对 临时列表和  递归调用结果进行  数组合并 返回
+		return operationListAll;
 	}
 }
